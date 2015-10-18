252a9b80bbb511fcff31bb268dbacccb04ce9cb8
hermaa02 <aaron.herman@luther.edu>
2015-10-18 12:47:20 -0500

    Update lower.c
    prints everything in array for now

--- lower.c
+++ lower.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <string.h>
+/* this will read from standard input and lowercase */
 
+void lower(char* arr) {
+  
+  int len = strlen(arr);
+  for (int k=0; k<len; k++) {
+    printf("%c",arr[k]);
+  }
+}

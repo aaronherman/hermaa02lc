9f2e190e486a82106ed0e185838d7f9ce6a0de5c
hermaa02 <aaron.herman@luther.edu>
2015-10-18 12:56:48 -0500

    Update lc.sh

--- lc.sh
+++ lc.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+for filename in *.txt
+do
+  if [ -f "$filename"]
+  then
+    #if filename is in the directory, do work
+  else
+    #script should print a usage statement to the screen along with documentation on how to use lc
+  fi
+done

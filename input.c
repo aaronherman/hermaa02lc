80adef638a437a9ce2e94b332e991d59742e1fa4
hermaa02 <aaron.herman@luther.edu>
2015-10-20 14:07:15 -0500

    Update input.c

--- input.c
+++ input.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-#include "input.c"
+#include "lower.h"
 
 int main(int argc, char* argv[]) {
-
+  char c;
+  
+  while ((c=getchar()) != EOF {
+    if (!lower(c))
+      printf("%c",c);
+  }
 }

0eca05f6457b64419bc81db06b694c2dc98ed00e
hermaa02 <aaron.herman@luther.edu>
2015-10-20 13:22:30 -0500

    Update lc.sh

--- lc.sh
+++ lc.sh
@@ -5,6 +5,7 @@ do
   if [ -f "$filename"]
   then
     #if filename is in the directory, do work
+    #cat file.txt | cprogram
   else
     #script should print a usage statement to the screen along with documentation on how to use lc
   fi

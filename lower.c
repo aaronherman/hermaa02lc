06770324cd3936c23f285ddb048b5de7d7742170
hermaa02 <aaron.herman@luther.edu>
2015-10-20 14:02:05 -0500

    Update lower.c

--- lower.c
+++ lower.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-#include "input.c"
 
-int main(int argc, char* argv[]) {
+/* when called, this will  subtract 32 from the variable*/
 
+void lower(char* arr) {
+  
+  int len = strlen(arr);
+  for (int k=0; k<len; k++) {
+    printf("%c",arr[k]);
+  }
 }
+
+
+
